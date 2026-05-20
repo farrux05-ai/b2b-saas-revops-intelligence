6c9a8c9076dca119a1aaf97470972b6bd3f9df85
Farrux Valijov <farruxbekvalijonov65@gmail.com>
2026-05-20 11:26:33 +0500

    feat: configure on_schema_change='sync_all_columns' for incremental models

--- models/marts/product/fct_feature_usage.sql
+++ models/marts/product/fct_feature_usage.sql
@@ -9,7 +9,8 @@
 {{ config(
     materialized='incremental',
     unique_key='usage_id',
-    incremental_strategy='merge'
+    incremental_strategy='merge',
+    on_schema_change='sync_all_columns'
 ) }}
 
 with events as (
